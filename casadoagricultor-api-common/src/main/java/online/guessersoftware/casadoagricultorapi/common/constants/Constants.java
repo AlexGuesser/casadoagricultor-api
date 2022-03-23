c93759c017194e4606ba998e9369b25f21f975c6
Alex Guesser <alex.guesser@hotmail.com>
2022-03-23 09:29:10 -0300

    Adjusts

---
+++ casadoagricultor-api-common/src/main/java/online/guessersoftware/casadoagricultorapi/common/constants/Constants.java
@@ -0,0 +1,14 @@
+package online.guessersoftware.casadoagricultorapi.common.constants;
+
+public class Constants {
+
+	public static final String DATE_REGEX = "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$";
+	public static final String NUMBERS = "0123456789";
+	public static final String TECH_JOB_PROCESSOR_NAME = "tech_job_processor";
+	public static final String DOT = ".";
+	public static final String SLASH = "-";
+	public static final String PDF = "pdf";
+	public static final String DEFAULT_DESTINY_FOLDER = "/home/alex/Desktop/Senai/TCC/Projects/pdfs/";
+	public static final String DEFAULT_BASE_URL = "https://www.ceasa.sc.gov.br/index.php/cotacao-de-precos/";
+
+}
---
+++ casadoagricultor-api-web-service/src/main/java/online/guessersoftware/casadoagricultorapi/webservice/CasadoagricultorApplication.java
@@ -0,0 +1,13 @@
+package online.guessersoftware.casadoagricultorapi.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "online.guessersoftware.casadoagricultorapi")
+public class CasadoagricultorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadoagricultorApplication.class, args);
+	}
+
+}
