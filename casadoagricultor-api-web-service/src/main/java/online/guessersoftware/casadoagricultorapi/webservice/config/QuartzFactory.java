cb3d5394ff18506b9adcb1d1091ff7512667ca9e
Alex Guesser <alex.guesser@hotmail.com>
2022-04-26 17:57:26 -0300

    Adjusts

---
+++ casadoagricultor-api-web-service/src/main/java/online/guessersoftware/casadoagricultorapi/webservice/config/QuartzFactory.java
@@ -0,0 +1,18 @@
+package online.guessersoftware.casadoagricultorapi.webservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+@Configuration
+public class QuartzFactory {
+
+	@Bean
+	public SchedulerFactoryBean schedulerFactoryBean() {
+		SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+		schedulerFactoryBean.setConfigLocation(new ClassPathResource("quartz.properties"));
+		return schedulerFactoryBean;
+	}
+
+}
