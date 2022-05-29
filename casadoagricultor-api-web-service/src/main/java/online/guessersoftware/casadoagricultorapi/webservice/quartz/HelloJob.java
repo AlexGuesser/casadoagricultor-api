91efdfaff250dff774ed613e7dcf40890fd66877
Alex Guesser <alex.guesser@hotmail.com>
2022-05-29 17:39:50 -0300

    Commenting out everything about Quartz

--- casadoagricultor-api-web-service/src/main/java/online/guessersoftware/casadoagricultorapi/webservice/quartz/HelloJob.java
+++ casadoagricultor-api-web-service/src/main/java/online/guessersoftware/casadoagricultorapi/webservice/quartz/HelloJob.java
@@ -1,14 +1,14 @@
-package online.guessersoftware.casadoagricultorapi.webservice.quartz;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
-public class HelloJob implements Job {
-
-	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		System.out.println("Estou rodando pelo Quartz às: " + System.currentTimeMillis());
-	}
-
-}
+//package online.guessersoftware.casadoagricultorapi.webservice.quartz;
+//
+//import org.quartz.Job;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//
+//public class HelloJob implements Job {
+//
+//	@Override
+//	public void execute(JobExecutionContext context) throws JobExecutionException {
+//		System.out.println("Estou rodando pelo Quartz às: " + System.currentTimeMillis());
+//	}
+//
+//}
